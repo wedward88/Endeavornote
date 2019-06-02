bcb0b7f88628a4419460a14587a4b532fac4eb72
wdunn <williamdunn195@gmail.com>
2019-06-02 00:07:55 -0400

    Finish modal and add preliminary styling, start styling notebook index

--- frontend/components/main/notebooks/NotebookFormContainer.jsx
+++ frontend/components/main/notebooks/NotebookFormContainer.jsx
@@ -1,25 +1,73 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createNotebook } from '../../../actions/notebook_actions';
 
 class NotebookForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { name: '' }
+        this.state = { name: '', user_id: this.props.user_id }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    
+    handleChange(field) {
+        return (e) => {
+            this.setState({ [field]: e.target.value });
+        }
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.createNotebook(this.state);
+        this.props.closeModal();
+    }
+
+    render () {
+        return (
+            <div className="modal-container">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-top">
+                        <div className="form-top-title-container">
+                            <h1>Create new notebook</h1>
+                            <i onClick={this.props.closeModal} className="fas fa-times fa-2x"></i>
+                        </div>
+                        <p>
+                            Notebooks are useful for grouping notes around a common topic. They can be private or shared.
+                        </p>
+                    </div>
+                    <div className="modal-form-input">
+                        
+                        <label><span>Name</span>
+                            <input 
+                            type="text"
+                            value={this.state.name}
+                            placeholder='Notebook name'
+                            onChange={this.handleChange('name')}
+                            />
+                        </label>
+                    </div>
+                    <div className="modal-buttons-container">
+                        <button onClick={this.props.closeModal} type="button">Cancel</button>
+                        <button className="modal-submit-button" type="submit">Continue</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
 }
 
 
 const msp = (state) => {
     return {
-
+        user_id: state.session.currentUserId
     }
 }
 
-const mdp = (dispatch) => {
+const mdp = (dispatch, ownProps) => {
     return {
-
+        createNotebook: (formInfo) => dispatch(createNotebook(formInfo)),
+        closeModal: () => ownProps.closeModal()
     }
 }
 
--- frontend/components/main/notebooks/NotebookIndex.jsx
+++ frontend/components/main/notebooks/NotebookIndex.jsx
@@ -1,45 +1,59 @@
 import React from 'react';
+import NotebookFormContainer from './NotebookFormContainer';
 
 class NotebookIndex extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = { mounted: false, modalOpen: false }
+        this.newNotebook = this.newNotebook.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
     
-    componentDidMount () {
+    componentDidMount() {
         this.props.retreiveNotebooks(this.props.user);
         this.setState({ mounted: true })
     }
 
-    newNotebook () {
-        this.setState({ mounted: this.state.mounted, modalOpen: true })
+    newNotebook() {
+        this.setState({ mounted: this.state.mounted, modalOpen: true });
+    }
+
+    closeModal() {
+        this.setState({ mounted: this.state.mounted, modalOpen: false });
     }
 
     render () {
         
-        let allNotebooks;
+        let tableData;
         let modalForm;
         if (this.state.mounted) {
-            allNotebooks = this.props.notebooks.map((notebook)=> {
+            tableData = this.props.notebooks.map((notebook)=> {
                 return <li key={notebook.id}>{notebook.name}</li>
             });
         }
 
         if (this.state.modalOpen) {
-            modalForm = <NotebookFormContainer />
+            modalForm = <NotebookFormContainer closeModal={this.closeModal} />
         }
+
         return (
             <section className="notebooks-index">
                 {modalForm}
                 <h1>Notebooks</h1>
-                <div class="index-sub-header">
+                <div className="index-sub-header">
                     <h2>My notebook list</h2>
-                    <h2>New Notebook</h2>
+                    <h2 onClick={this.newNotebook}>New Notebook</h2>
                 </div>
                 <ul>
-                    {allNotebooks}
+                    {/* {allNotebooks} */}
                 </ul>
+                <table>
+                    <tr>
+                        <th>Title</th><th>Created By</th><th>Updated</th><th>Actions</th>
+                    </tr>
+                    {tableData}
+                </table>
             </section>
         )
     }
