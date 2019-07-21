c409a36e63ee29a7f53e7361fced29fd24f77e45
wdunn <williamdunn195@gmail.com>
2019-07-21 18:28:29 -0400

    Build out tags reducer

---
+++ frontend/reducers/entities/tags_reducer.js
@@ -0,0 +1,29 @@
+import { RECEIVE_TAGS, RECEIVE_ONE_TAG, DELETE_TAG } from '../../actions/tag_actions';
+import { merge } from 'lodash';
+
+const tagsReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+
+        case RECEIVE_TAGS:
+            const newTags = {};
+            Object.values(action.tags).forEach((tag) => {
+                newTags[tag.id] = tag;
+            });
+            return merge({}, state, newTags);
+
+        case RECEIVE_ONE_TAG:
+            return merge({}, state, { [action.tag.id]: action.tag });
+
+        case DELETE_TAG:
+            const newState = merge({}, state);
+            delete newState[action.tag.id];
+            return newState;
+        
+        default:
+            return state;
+    }
+}
+
+export default tagsReducer;
