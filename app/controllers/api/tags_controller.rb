d09152126ea4260b62e79887be4b607656a30393
wdunn <williamdunn195@gmail.com>
2019-07-23 14:51:01 -0400

    Fix issue with deleting tags

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -36,14 +36,16 @@ class Api::TagsController < ApplicationController
     def destroy
         @note = Note.find(params[:tag][:note_id])
         @tag = Tag.find(params[:id])
-        if @ta== current_user.id
-            if @note.tags.destroy(@tag)
+
+        if @tag.user_id == current_user.id
+            if @note.taggings.where(tag_id: @tag.id).delete_all
                 @tag.destroy
                 render json: @tag
             else
                 render json: @tag.errors.full_messages, status: 418
             end
         else
+            debugger
             render json: ['This is not your tag to delete!'], status: 418
         end
     end
